b3f53b78a3a4cfe8b7cbb5e2d48ec17cabc42c91
Agustinus Ardian Cakra Widiastara <ardiancakra89@gmail.com>
2022-12-12 15:26:40 +0700

    Adding home page and asset

---
+++ resources/views/home.blade.php
@@ -0,0 +1,85 @@
+@extends('layout.master')
+@section('content')
+    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/RCAhiGJsUUE/1920x1080')">
+                <div class="carousel-caption">
+                    <h5>First slide label</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item" style="background-image: url('https://source.unsplash.com/wfh8dDlNFOk/1920x1080')">
+                <div class="carousel-caption">
+                    <h5>Second slide label</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                </div>
+            </div>
+            <div class="carousel-item" style="background-image: url('/posters/Avengers Endgame.jpg')">
+                <div class="carousel-caption">
+                    <h5>Third slide label</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    <div class="container">
+        <h3 class="subtitle"><b>Trending Now</b></h3>
+        <div class="cards">
+                <div class="card" style="width: 18rem;">
+                    <a href="">
+                    <img class="card-img-top" src="/posters/Avengers Endgame.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">TITLE</h5>
+                        <p class="card-text">by<br>ASJKFDJK</p>
+                    </div>
+                    </a>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <a href="">
+                    <img class="card-img-top" src="/posters/Moonlight.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">TITLE</h5>
+                        <p class="card-text">by<br>ASJKFDJK</p>
+                    </div>
+                    </a>
+                </div>
+        </div>
+        <h3 class="subtitle"><b>Trending Now</b></h3>
+        <div class="cards">
+            <div class="card" style="width: 18rem;">
+                <a href="">
+                    <img class="card-img-top" src="/posters/Avengers Endgame.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">TITLE</h5>
+                        <p class="card-text">by<br>ASJKFDJK</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="card" style="width: 18rem;">
+                <a href="">
+                    <img class="card-img-top" src="/posters/Moonlight.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">TITLE</h5>
+                        <p class="card-text">by<br>ASJKFDJK</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+@endsection
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,21 +1,26 @@
 <div class="outer">
-    <div class="container">
-        <h1>Giant Book Supplier</h1>
-    </div>
-</div>
-<div class="container navbar">
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><div class="dropdown">
-                <button onclick="showCategories()" class="dropbtn">Categories</button>
-                <div class="dropdown-content" id="categories">
-                    @foreach($categories as $ctg)
-                        <a href="{{asset("category/$ctg->id")}}">{{ $ctg->name }}</a>
-                    @endforeach
+    <div class="container navbar">
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><div class="dropdown">
+                    <button onclick="showCategories()" class="dropbtn">Categories</button>
+                    <div class="dropdown-content" id="categories">
+                        {{--                    @foreach($categories as $ctg)--}}
+                        {{--                        <a href="{{asset("category/$ctg->id")}}">{{ $ctg->name }}</a>--}}
+                        {{--                    @endforeach--}}
+                        <a href="">Category</a>
+                        <a href="">Category</a>
+                        <a href="">Category</a>
+                    </div>
                 </div>
-            </div>
-        </li>
-        <li><a href="{{asset('publisher')}}">Publisher</a></li>
-        <li><a href="{{asset('contact')}}">Contact</a></li>
-    </ul>
+            </li>
+            <li><a href="">Movies</a></li>
+            <li><a href="">Series</a></li>
+            <li><a href="">Anime</a></li>
+        </ul>
+        <ul>
+            <li><input type="text" placeholder="Search.."></li>
+            <li><a href="">Sign in</a></li>
+        </ul>
+    </div>
 </div>
