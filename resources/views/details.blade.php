0f2c52863a197a665a37524aacaf9a40b8ca93a9
Agustinus Ardian Cakra Widiastara <ardiancakra89@gmail.com>
2022-12-14 00:35:42 +0700

    Add reccomendation in detail page

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -19,4 +19,28 @@
             <img  class="poster" src="/posters/John Wick Parabellum.jpg" alt="Poster">
         </div>
     </div>
+    <div class="background">
+        <div class="container">
+            <h3 class="subtitle"><b>You might also like...</b></h3>
+            <div class="cards">
+                <div class="card" style="width: 18rem;">
+                    <a href="{{asset('details')}}">
+                        <img class="card-img-top" src="/posters/Avengers Endgame.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">TITLE</h5>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="card" style="width: 18rem;">
+                    <a href="{{asset('details')}}">
+                        <img class="card-img-top" src="/posters/Moonlight.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">TITLE</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
